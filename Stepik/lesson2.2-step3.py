3694f5ce4bcdf4fb6bca367b1e6ee6c1710e89f2
jeimaster <jeimaster@yandex.ru>
2024-09-02 08:05:11 +0300

    Добасление комментариев и незначительные правки

--- Stepik/lesson2.2-step3.py
+++ Stepik/lesson2.2-step3.py
@@ -10,17 +10,16 @@ browser.get(link) # Открытие ссылки
 
 
 try:
-    num1 = int(browser.find_element(By.ID, "num1").text)
-    print(num1)
-    num2 = int(browser.find_element(By.ID, "num2").text)
-    print(num2)
-    answer = num1 + num2
+    num1 = int(browser.find_element(By.ID, "num1").text) # Находим первое число и забираем текст из него -> Переводим в инт
+    num2 = int(browser.find_element(By.ID, "num2").text) # Находим второе число и забираем текст из него -> Переводим в инт
+
+    answer = num1 + num2 # Складываем данные числа для ответа
 
 
     select_list = Select(browser.find_element(By.TAG_NAME, "select")) # Сначала ищем Элемент (Выпадающий список) и затем выбираем его
     select_list.select_by_value(str(answer)) # Выбираем элемент выпадающего списка по значению 48
 
-    submit_button = browser.find_element(By.CLASS_NAME, "btn").click() # Находим кнопку Submit
+    submit_button = browser.find_element(By.CLASS_NAME, "btn").click() # Находим кнопку Submit и кликаем по ней
 
 
 finally:
