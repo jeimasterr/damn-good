9fc4739589f97f3eca11535564328a5648c8df92
jeimaster <jeimaster@yandex.ru>
2024-09-03 10:01:25 +0300

    Файл дополнен комментариями

--- Stepik/lesson2.3-step4.py
+++ Stepik/lesson2.3-step4.py
@@ -9,7 +9,7 @@ link = "http://suninjuly.github.io/alert_accept.html"
 browser = webdriver.Chrome()
 browser.get(link)
 
-
+# Функция для подсчета по формуле
 def calc(x):
     return log(abs(12*sin(x)))
 
@@ -19,17 +19,17 @@ try:
     submit_button = browser.find_element(By.CLASS_NAME, "btn")  # Находим кнопку Submit
     submit_button.click()
 
-    confirm = browser.switch_to.alert
-    confirm.accept()
+    confirm = browser.switch_to.alert # Переключаемся на всплывающее окно
+    confirm.accept() # Принимаем всплывающее окно
 
 
-    x_element = int(browser.find_element(By.ID, "input_value").text)
-    x = calc(x_element)
+    x_element = int(browser.find_element(By.ID, "input_value").text) # Находим значение элемента x и преобразовываем в инт
+    x = calc(x_element)                                              # Обращаемся к функции calc для подсчета по формуле
 
-    input_value = browser.find_element(By.ID, "answer")
+    input_value = browser.find_element(By.ID, "answer") # Ищем поле для ввода значения
     input_value.send_keys(x)
 
-    Submit = browser.find_element(By.CLASS_NAME, "btn")
+    Submit = browser.find_element(By.CLASS_NAME, "btn") # Ищем кнопку Submit
     Submit.click()
 
 
